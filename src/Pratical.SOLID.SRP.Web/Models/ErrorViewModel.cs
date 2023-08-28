4782ca34746ac2277f6dd247c819bf1bbfcc145c
mateus.silva <mateus.silva@iatec.com>
2023-08-28 08:17:13 -0300

    Add mvc SRP

---
+++ src/Pratical.SOLID.SRP.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Pratical.SOLID.SRP.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
